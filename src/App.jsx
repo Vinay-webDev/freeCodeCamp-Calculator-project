98dd35b56bc94a067fa3cfd50ea5f4d1cd5bfc4e
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-08-11 21:11:11 +0530

    Key and Keyboard components

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const operators = ['AC', '/', 'X', '-', '+', '=', '.'];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 
 //Display component
-const Display = (props = { input, output }) => {
+const Display = ({ input, output }) => {
   return(
     <div className="output" >
       <span className="result" >{output}</span>
@@ -33,17 +33,42 @@ const Display = (props = { input, output }) => {
     </div>
   );
 }
+//Key component
+const Key = ({ keyData: {id, value}, handleInput }) => {
+  return(
+    <button id={id} handleInput={() => {handleInput}} >{value}</button>
+  );
+}
+
+
+//Keyboard component
+const Keyboard = ({ handleInput }) => {
+  return(
+      <div classaName="keys" >
+        {Data.map((key) => (<Key key={key.id} keyData={key} handleInput={handleInput} />))}
+      </div>
+  );
+}
+
+
+
+
 
 
 function App() {
   
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState();
   const [output, setOutput] = useState();
+
+  const handleInput = () => {
+
+  }
 
   return (
     <div className="container">
       <div className="calculator">
         <Display input={input} output={output} />
+        <Keyboard handleInput={handleInput}/>
       </div>
     </div>
   );
